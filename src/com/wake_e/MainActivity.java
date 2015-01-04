05ebfbca0d319026c195e3a6ef895ce1cd885adb
Trolledo <nugetchar@gmail.com>
2015-01-04 01:49:22 -0500

    Slides deleguees au SlidesManager

--- src/com/wake_e/MainActivity.java
+++ src/com/wake_e/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wake_e;
 
+<<<<<<< Updated upstream
 import android.R;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -37,5 +38,92 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+=======
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.wake_e.adapt.MyPagerAdapter;
+import com.wake_e.services.managers.SlidesManager;
+
+public class MainActivity extends FragmentActivity {
+
+
+    private PagerAdapter mPagerAdapter;
+
+
+    public static MainActivity that;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	that = this;
+	/*
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.station);
+		FontUtil.overrideFont(getApplicationContext(), "future", "fonts/fufure.ttf");
+
+		List<String> mails = new ArrayList<String>();
+
+		mails.add("Mail 1");
+		mails.add("Mail 2");
+		mails.add("Mail 3");
+
+		ListView gridview = (ListView) findViewById(R.id.content);
+	    gridview.setAdapter(new MailAdapter(this,mails));
+	 */
+	super.onCreate(savedInstanceState);
+	super.setContentView(R.layout.viewpager);
+
+	// Creation de la liste de Fragments que fera defiler le PagerAdapter
+	//List<Fragment> fragments = new Vector<Fragment>();
+	
+	/**
+	 * TTO : J'ai deplace ce code dans le try...catch de loadSlidesFile() de SlidesManager
+	 */
+	// Ajout des Fragments dans la liste
+	//		fragments.add(Fragment.instantiate(this,PageHomePageFragment.class.getName()));
+	//		fragments.add(Fragment.instantiate(this,PageReveilFragment.class.getName()));
+	//		fragments.add(Fragment.instantiate(this,PageMailFragment.class.getName()));
+	//		fragments.add(Fragment.instantiate(this,PageAgendaFragment.class.getName()));
+	//		fragments.add(Fragment.instantiate(this,PageMeteoFragment.class.getName()));
+
+	// Creation de l'adapter qui s'occupera de l'affichage de la liste de
+	// Fragments
+	List<Fragment> fragments = SlidesManager.getInstance(this).getAllFragments();
+	this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments);
+
+	ViewPager pager = (ViewPager) super.findViewById(R.id.pager);
+	// Affectation de l'adapter au ViewPager
+	pager.setAdapter(this.mPagerAdapter);
+
+	
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle action bar item clicks here. The action bar will
+	// automatically handle clicks on the Home/Up button, so long
+	// as you specify a parent activity in AndroidManifest.xml.
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
+	    return true;
+	}
+	return super.onOptionsItemSelected(item);
+>>>>>>> Stashed changes
     }
 }
--- src/com/wake_e/services/managers/SlidesManager.java
+++ src/com/wake_e/services/managers/SlidesManager.java
@@ -1,8 +1,21 @@
 package com.wake_e.services.managers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Vector;
 
-import android.view.View;
+import android.content.Context;
+import android.os.Environment;
+import android.support.v4.app.Fragment;
+
+import com.wake_e.fragment.PageHomePageFragment;
+import com.wake_e.fragment.PageReveilFragment;
+import com.wake_e.fragment.station.PageAgendaFragment;
+import com.wake_e.fragment.station.PageMailFragment;
+import com.wake_e.fragment.station.PageMeteoFragment;
 
 /**
  * @brief Used to manager the slides in the morning
@@ -12,28 +25,89 @@ import android.view.View;
 public class SlidesManager {
     // une liste ordonnée des vues visibles dans le slide. Paramétrable
     // via les paramètres globaux.
-    private List<View> visibleViews;
+    private List<Fragment> visibleViews;
+    private Fragment homePage;
+
+    private static SlidesManager manager;
 
     /**
      * 
      */
-    public SlidesManager() {
+    private SlidesManager() {
 	super();
+	visibleViews = new Vector<Fragment>();
+    }
+
+    private SlidesManager(Context context){
+	this();
+	homePage = Fragment.instantiate(context,PageHomePageFragment.class.getName());
     }
 
     /**
      * @return slide's views
      */
-    public List<View> getVisibleViews() {
+    public List<Fragment> getVisibleViews() {
 	return this.visibleViews;
+    }
+    
+    /**
+     * @return all fragments (slide's views + home page)
+     */
+    public List<Fragment> getAllFragments(){
+	List<Fragment> fragments = new Vector<Fragment>();
+	fragments.add(this.homePage);
+	fragments.addAll(this.visibleViews);
+	return fragments;
     }
 
     /**
-     * @param views
-     *            the views for the slide
+     * @brief charger le fichier contenant les slides que l'utilisateur veut
+     *        voir
+     * @param context
+     *            le contexte
      */
-    public void setVisibleViews(List<View> views) {
-	this.visibleViews = views;
+    public void loadSlidesFile(Context context) {
+
+	// Empty the fragments list
+	this.visibleViews.clear();
+	this.visibleViews.add(Fragment.instantiate(context, PageHomePageFragment.class.getName()));
+
+	// Find the directory for the SD Card using the API
+	// *Don't* hardcode "/sdcard"
+	File sdcard = Environment.getExternalStorageDirectory();
+
+	// Get the text file
+	File file = new File(sdcard + "/wake_e", "slides.txt");
+
+	try {
+	    BufferedReader br = new BufferedReader(new FileReader(file));
+	    String line;
+
+	    // A line represent a class name
+	    // e.g : com.wake_e.fragment.PageHomePageFragment
+	    while ((line = br.readLine()) != null) {
+		visibleViews.add(Fragment.instantiate(context, line));
+	    }
+	    br.close();
+	} catch (IOException e) {
+	    // A la moindre erreur on charge tout automatiquement
+	    this.visibleViews.clear();
+	    this.visibleViews.add(Fragment.instantiate(context,PageReveilFragment.class.getName()));
+	    this.visibleViews.add(Fragment.instantiate(context,PageMailFragment.class.getName()));
+	    this.visibleViews.add(Fragment.instantiate(context,PageAgendaFragment.class.getName()));
+	    this.visibleViews.add(Fragment.instantiate(context,PageMeteoFragment.class.getName()));
+	}
     }
 
+    public static SlidesManager getInstance(Context context) {
+	if (SlidesManager.manager == null) {
+	    SlidesManager.manager = new SlidesManager(context);
+	    SlidesManager.manager.loadSlidesFile(context);
+	}
+	return SlidesManager.manager;
+    }
+
+    public Fragment getHomePage() {
+	return homePage;
+    }
 }
