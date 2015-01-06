a7f6bac4d6ded1a1268d607b9c64a089c6a01f88
EmilienNotarianni <emilien.notarianni@gmail.com>
2015-01-06 13:13:43 +0100

    Meteo V0

--- src/com/wake_e/adapt/MeteoAdapter.java
+++ src/com/wake_e/adapt/MeteoAdapter.java
@@ -1,8 +1,11 @@
 package com.wake_e.adapt;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
+import com.wake_e.MainActivity;
 import com.wake_e.R;
+import com.wake_e.model.Meteo;
 
 
 import android.content.Context;
@@ -10,20 +13,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MeteoAdapter extends BaseAdapter {
-	List<String> meteos;
+	List<Meteo> meteos;
 	LayoutInflater inflater;
     int index;
     ViewHolder holder;
 
-	public MeteoAdapter(Context context,List<String> tasks) {
+	public MeteoAdapter(Context context,List<Meteo> meteos) {
 	    inflater = LayoutInflater.from(context);
-	    this.meteos = tasks;
+	    this.meteos = meteos;
 	}
-	public void setTasks(List<String> tasks){
-		this.meteos = tasks;
+	public void setTasks(List<Meteo> meteos){
+		this.meteos = meteos;
 	}
 	@Override
 	public int getCount() {
@@ -41,7 +45,11 @@ public class MeteoAdapter extends BaseAdapter {
 	}
 
 	private class ViewHolder {
-	    TextView subjet;
+	    TextView city;
+	    TextView temp;
+	    TextView humi;
+	    TextView wind;
+	    ImageView icon;
 	}
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -50,11 +58,34 @@ public class MeteoAdapter extends BaseAdapter {
 
 	    if(convertView == null) {
 	        holder = new ViewHolder();
-	        convertView = inflater.inflate(R.layout.mail, null);
+	        convertView = inflater.inflate(R.layout.meteo, null);
+	        
+	        DecimalFormat df = new DecimalFormat ( ) ; 
+	        df.setMaximumFractionDigits ( 2 ) ;
+	        df.setMinimumFractionDigits ( 2 ) ; 
+	        df.setDecimalSeparatorAlwaysShown ( true ) ; 
+	        System.out.println ( Double.parseDouble(df.format ( -4.327)));
 
-	        holder.subjet 	= (TextView)convertView.findViewById(R.id.mailFrom);
-	        holder.subjet.setText(meteos.get(position));
-
+	        holder.city 	= (TextView)convertView.findViewById(R.id.city);
+	        holder.temp 	= (TextView)convertView.findViewById(R.id.temperature);
+	        holder.humi 	= (TextView)convertView.findViewById(R.id.humidity);
+	        holder.wind 	= (TextView)convertView.findViewById(R.id.wind);
+	        holder.icon 	= (ImageView)convertView.findViewById(R.id.icon);
+	        
+	        holder.city.setText(meteos.get(position).getCity());
+	        holder.temp.setText(
+	        		Double.parseDouble(df.format (
+	        				meteos.get(position).getTemperature()))
+	        		+ " °C");
+	        holder.humi.setText("Humidité " + 
+	        		Double.parseDouble(df.format (
+	        				meteos.get(position).getHumidite()))
+	        		+ " %");
+	        holder.wind.setText("Vent " + Double.parseDouble(df.format (
+	        		meteos.get(position).getWindStrength()))
+	        		+ " hm/h");
+	        holder.icon.setImageResource(convertView.getResources().getIdentifier("d" + meteos.get(position).getIcon(), "drawable", MainActivity.that.getPackageName()));
+	        holder.icon 	= (ImageView)convertView.findViewById(R.id.icon);
 	        convertView.setTag(holder);
 	    } else {
 	        holder = (ViewHolder) convertView.getTag();
--- src/com/wake_e/fragment/station/PageMeteoFragment.java
+++ src/com/wake_e/fragment/station/PageMeteoFragment.java
@@ -3,11 +3,16 @@ package com.wake_e.fragment.station;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONException;
+
 import com.wake_e.R;
-import com.wake_e.adapt.MailAdapter;
 import com.wake_e.adapt.MeteoAdapter;
+import com.wake_e.model.Meteo;
+import com.wake_e.tools.JSONWeatherParser;
+import com.wake_e.tools.WeatherHttpClient;
 
 import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,22 +22,48 @@ import android.widget.TextView;
 
 public class PageMeteoFragment extends Fragment {
 
+	View v;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.station, container, false);
+		v = inflater.inflate(R.layout.station, container, false);
 		if (v != null){
-			List<String> meteos = new ArrayList<String>();
-			
-			meteos.add("Météo 1");
-			meteos.add("Météo 2");
+			String city = "Paris";
 
 			TextView title = (TextView) v.findViewById(R.id.title_station);
 			title.setText(v.getContext().getString(R.string.meteo));
 			
-			ListView gridview = (ListView) v.findViewById(R.id.content);
-		    gridview.setAdapter(new MeteoAdapter(v.getContext(),meteos));
+		    JSONWeatherTask task = new JSONWeatherTask();
+		    task.execute(new String[]{city});
 		}
 		return v;/*inflater.inflate(R.layout.station, container, false);*/
+	}
+	private class JSONWeatherTask extends AsyncTask<String, Void, Meteo> {
+		
+		@Override
+		protected Meteo doInBackground(String... params) {
+			Meteo weather = new Meteo();
+			String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+			try {
+				weather = JSONWeatherParser.getWeather(data);
+								
+			} catch (JSONException e) {				
+				e.printStackTrace();
+			}
+			return weather;
+		}
+		
+		
+	@Override
+		protected void onPostExecute(Meteo weather) {
+			super.onPostExecute(weather);
+			if (v != null){
+				List<Meteo> meteos = new ArrayList<Meteo>();
+				meteos.add(weather);
+				ListView gridview = (ListView) v.findViewById(R.id.content);
+			    gridview.setAdapter(new MeteoAdapter(v.getContext(),meteos));
+			}
+		}
 	}
 }
