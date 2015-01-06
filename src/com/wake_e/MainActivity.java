fedd69f549d110829b884070fbe0996a9570a160
Trolledo <nugetchar@gmail.com>
2015-01-06 10:46:30 -0500

    blublu

--- src/com/wake_e/MainActivity.java
+++ src/com/wake_e/MainActivity.java
@@ -2,6 +2,7 @@ package com.wake_e;
 
 import java.util.List;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,8 +11,10 @@ import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -63,6 +66,8 @@ public class MainActivity extends FragmentActivity {
 	ll.setY(0);
 	pager.setLayoutParams(new LinearLayout.LayoutParams(
 		LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	ImageView settings = (ImageView) findViewById(R.id.parametre);
+	settings.setOnClickListener(switchToSettings);
 
     }
 
@@ -132,5 +137,15 @@ public class MainActivity extends FragmentActivity {
 	    // v.getY() - relative.getHeight()));
 	    return true;
 	}
+    };
+
+    private OnClickListener switchToSettings = new OnClickListener() {
+
+	@Override
+	public void onClick(View v) {
+	    Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+	    startActivity(i);
+	}
+
     };
 }
