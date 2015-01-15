d7ae848025dabc5ccfd12752548a5234dc32c8e3
Trolledo <nugetchar@gmail.com>
2015-01-15 17:42:06 -0500

    NPE fixed

--- src/com/wake_e/services/deliverers/MeteoDeliverer.java
+++ src/com/wake_e/services/deliverers/MeteoDeliverer.java
@@ -3,17 +3,14 @@ package com.wake_e.services.deliverers;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 
 import org.json.JSONException;
 
 import android.os.AsyncTask;
-import android.widget.ListView;
+import android.view.View;
 
 import com.wake_e.Controller;
-import com.wake_e.R;
-import com.wake_e.adapt.MeteoAdapter;
 import com.wake_e.fragment.station.PageMeteoFragment;
 import com.wake_e.model.Location;
 import com.wake_e.model.Meteo;
@@ -46,8 +43,8 @@ public class MeteoDeliverer{
     	this.view = view;
     	meteos = new ArrayList<Meteo>();
 	    JSONWeatherTask task = new JSONWeatherTask();
-	    
-	    Set<Location> l = Controller.getInstance(view.getView().getContext()).getLocations();
+	    View v = view.getView();
+	    Set<Location> l = Controller.getInstance(Controller.getContext()).getLocations();
 	    Iterator<Location> iterator = l.iterator();
 	    while(iterator.hasNext()) {
 	        Location setElement = iterator.next();
